429d5941f3fdb9665af443cf3c5be67bd59a5aef
sayali17 <118358501+sayali17@users.noreply.github.com>
2022-11-29 18:02:17 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    // typing text animation script
+        var typed = new Typed(".typing", {
+            strings: ["Developer.", "Programmer.", "Designer.", "Freelancer.","Draughtsman."],
+            typeSpeed: 100,
+            backSpeed: 60,
+            loop: true
+        });
+
+});
